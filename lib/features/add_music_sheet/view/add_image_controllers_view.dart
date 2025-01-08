80f28f434a238044733f4791df54bb938035a049
Pavol Vlcek <pavol.vlcek@daktela.com>
2025-01-08 09:03:07 +0100

    moved picker to AddImageControllersView, used expanded to dynamically resize

--- lib/features/add_music_sheet/view/add_image_controllers_view.dart
+++ lib/features/add_music_sheet/view/add_image_controllers_view.dart
@@ -1,20 +1,17 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:organista/features/add_music_sheet/cubit/add_music_sheet_cubit.dart';
 import 'package:organista/views/download_image_view.dart';
 
-class AddImageControllersView extends StatelessWidget {
-  const AddImageControllersView({
-    super.key,
-    required this.picker,
-  });
-
-  final ImagePicker picker;
+class AddImageControllersView extends HookWidget {
+  const AddImageControllersView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final picker = useMemoized(() => ImagePicker(), [key]);
     return Row(
       children: [
         Expanded(
--- lib/features/add_music_sheet/view/add_music_sheet_view.dart
+++ lib/features/add_music_sheet/view/add_music_sheet_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:organista/blocs/app_bloc/app_bloc.dart';
 import 'package:organista/features/add_music_sheet/cubit/add_music_sheet_cubit.dart';
 import 'package:organista/features/add_music_sheet/view/add_image_controllers_view.dart';
@@ -17,17 +16,9 @@ class AddMusicSheetView extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final picker = useMemoized(() => ImagePicker(), [key]);
     final musicSheetNameController = useTextEditingController();
     return Scaffold(
-      appBar: AppBar(
-          title: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          color: Colors.grey[800], // Background color for the search box
-          borderRadius: BorderRadius.circular(8), // Rounded corners
-        ),
-      )),
+      appBar: AppBar(title: const Text('Upload the music sheet')),
       body: BlocBuilder<AddMusicSheetCubit, AddMusicSheetState>(
         builder: (context, state) {
           musicSheetNameController.text = state.fileName;
@@ -35,57 +26,56 @@ class AddMusicSheetView extends HookWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                SizedBox(
-                  height: 250,
-                  width: 500,
-                  child: (state.file == null) ? AddImageControllersView(picker: picker) : UploadedMusicSheetImageView(image: state.file!),
+                Expanded(
+                  flex: 2,
+                  child: (state.file == null) ? const AddImageControllersView() : UploadedMusicSheetImageView(image: state.file!),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: musicSheetNameController,
-                        decoration: const InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey, width: 0.0),
-                          ),
-                          hintText: 'Music sheet name',
-                          hintStyle: TextStyle(color: Colors.black),
+                Expanded(
+                  flex: 3,
+                  child: TextField(
+                    controller: musicSheetNameController,
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 0.0),
+                      ),
+                      hintText: 'Music sheet name',
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                    style: const TextStyle(color: Colors.black), // Text color
+                    onChanged: (query) {},
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (state.file != null) {
+                              context.read<AppBloc>().add(
+                                    AppEventUploadImage(
+                                      file: state.file!,
+                                      fileName: musicSheetNameController.text,
+                                    ),
+                                  );
+                              resetMusicSheetCubitAndPop(context);
+                            } else {
+                              CustomLogger.instance.e("You have to select an image first");
+                            }
+                          },
+                          child: const Text('Save'),
                         ),
-                        style: const TextStyle(color: Colors.black), // Text color
-                        onChanged: (query) {},
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (state.file != null) {
-                            context.read<AppBloc>().add(
-                                  AppEventUploadImage(
-                                    file: state.file!,
-                                    fileName: musicSheetNameController.text,
-                                  ),
-                                );
-                            context.read<AddMusicSheetCubit>().resetState();
-                            Navigator.pop(context);
-                          } else {
-                            CustomLogger.instance.e("You have to select an image first");
-                          }
-                        },
-                        child: const Text('Save'),
+                      Expanded(
+                        child: ElevatedButton(
+                          //TODO: add dialog do you really want to discard music sheet
+                          onPressed: () => resetMusicSheetCubitAndPop(context),
+                          child: const Text('Discard'),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('Discard'),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 )
               ],
             ),
@@ -93,5 +83,10 @@ class AddMusicSheetView extends HookWidget {
         },
       ),
     );
+  }
+
+  void resetMusicSheetCubitAndPop(BuildContext context) {
+    context.read<AddMusicSheetCubit>().resetState();
+    Navigator.pop(context);
   }
 }
