37f1364f5c8b65b1a4fcd7b6ff9d4de05faacce2
Pavol Vlcek <pavol.vlcek@daktela.com>
2025-01-12 15:56:23 +0100

    replaced ReorderableListView.builder with ReorderableListView constructor in main_view, removed cachedFutures and firebaseStorageRepository as dependencies

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,13 +1,10 @@
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:organista/blocs/app_bloc/app_bloc.dart';
 import 'package:organista/logger/custom_logger.dart';
-import 'package:organista/managers/image_cache_manager.dart';
 import 'package:organista/models/music_sheets/music_sheet.dart';
-import 'package:organista/repositories/firebase_storage_repository.dart';
 import 'package:organista/features/add_music_sheet/view/add_music_sheet_view.dart';
 import 'package:organista/views/main_popup_menu_button.dart';
 import 'package:organista/views/music_sheet_list_tile.dart';
@@ -21,11 +18,6 @@ class MainView extends HookWidget {
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final Color evenItemColor = colorScheme.primary.withOpacity(0.5);
-
-    // final List<Reference> images = context.watch<AppBloc>().state.images?.toList() ?? [];
-    // Cache Futures for images
-    final cachedFutures = useState<Map<Reference, Future<Uint8List?>>>(<Reference, Future<Uint8List?>>{});
-    final firebaseStorageRepository = context.read<FirebaseStorageRepository>();
 
     return Scaffold(
       appBar: AppBar(
@@ -42,24 +34,8 @@ class MainView extends HookWidget {
         builder: (context, state) {
           logger.i("Item count is ${state.musicSheets?.length}");
           var musicSheets = (state.musicSheets ?? []).toList();
-          return ReorderableListView.builder(
+          return ReorderableListView(
             padding: const EdgeInsets.only(top: 10),
-            itemCount: musicSheets.length,
-            itemBuilder: (context, index) {
-              final musicSheet = musicSheets.elementAt(index);
-              Reference image = firebaseStorageRepository.getReference(musicSheet.originalFileStorageId);
-              // Cache future for this image if not already cached
-              cachedFutures.value.putIfAbsent(image, () => ImageCacheManager().loadImage(image));
-
-              return MusicSheetListTile(
-                key: Key(musicSheet.musicSheetId),
-                musicSheet: musicSheet,
-                cachedFutures: cachedFutures,
-                image: image,
-                evenItemColor: evenItemColor,
-                musicSheets: musicSheets,
-              );
-            },
             onReorderStart: (_) => HapticFeedback.heavyImpact(),
             onReorder: (int oldIndex, int newIndex) {
               if (oldIndex < newIndex) {
@@ -69,6 +45,15 @@ class MainView extends HookWidget {
               musicSheets.insert(newIndex, item);
               context.read<AppBloc>().add(AppEventReorderMusicSheet(musicSheets: musicSheets));
             },
+            children: [
+              for (int index = 0; index < musicSheets.length; index += 1)
+                MusicSheetListTile(
+                  key: Key(musicSheets.elementAt(index).musicSheetId),
+                  musicSheet: musicSheets.elementAt(index),
+                  evenItemColor: evenItemColor,
+                  musicSheets: musicSheets,
+                )
+            ],
           );
         },
       ),
