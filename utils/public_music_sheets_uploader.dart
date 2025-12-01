38f87bcf459cea0c7ef6d6e6e3b74a1cdeb3c41b
Pavol Vlcek <pavol.vlcek@daktela.com>
2025-12-01 21:09:13 +0100

    fix music sheets uploader

--- utils/public_music_sheets_uploader.dart
+++ utils/public_music_sheets_uploader.dart
@@ -18,6 +18,7 @@ final firebaseFirestoreRepository = FirebaseFirestoreRepository();
 final firebaseStorageRepository = FirebaseStorageRepository();
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await firebaseInitialize();
   runApp(const PublicMusicSheetsUploader());
 }
