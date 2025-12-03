b86c0072c2aa00858887ff47228151a0136e237f
Pavol Vlcek <juststrawbery@gmail.com>
2025-12-03 12:15:31 +0100

    added CacheManager to RepositoryProvider context, pdf viewer widget and repository music sheet now uses CacheManager from context

--- lib/views/app_repository.dart
+++ lib/views/app_repository.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:organista/managers/persistent_cache_manager.dart';
 import 'package:organista/views/app.dart';
 import 'package:organista/repositories/firebase_firestore_repository.dart';
 import 'package:organista/repositories/firebase_storage_repository.dart';
@@ -23,6 +25,9 @@ class AppRepository extends StatelessWidget {
           create: (context) => SettingsRepository(
             context.read<SharedPreferencesWithCache>(),
           ),
+        ),
+        RepositoryProvider<CacheManager>(
+          create: (context) => PersistentCacheManager(),
         ),
       ],
       child: const App(),
