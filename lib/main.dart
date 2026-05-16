40654df0aa8d8cc3d2abec7901eab4faba10d93c
Pavol Vlcek <juststrawbery@gmail.com>
2026-05-16 13:30:36 +0200

    refactor: remove profilesSampleRate configuration from Sentry initialization

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,7 @@ Future<void> main() async {
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
       // We recommend adjusting this value in production.
       options.tracesSampleRate = 1.0;
-      // The sampling rate for profiling is relative to tracesSampleRate
-      // Setting to 1.0 will profile 100% of sampled transactions:
-      options.profilesSampleRate = 1.0;
+
       // Only capture errors in release mode
       options.environment = kDebugMode ? 'development' : 'production';
     },
